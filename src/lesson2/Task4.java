6606d8d501dfaa6a7d6849d427afb165b68e8e64
IvanBalahura <109754747+IvanBalahura@users.noreply.github.com>
2022-08-21 23:58:14 +0300

    c1

--- src/lesson2/Task4.java
+++ src/lesson2/Task4.java
@@ -1,27 +1,21 @@
 package lesson2;//Create two arrays of 10 integers.
 // Find the square root of each element in the first array and put the result of it to the second array. Print second array to the console.
-
 public class Task4 {
-    public static void main(String[] args){
-        double  [] x = {1.0,2.0,3.0,4.0,5.0,6.0,7.0,8.0,9.0,10.0};
-        double [] y = new double[10];
-        y[0]= Math.sqrt(x[0]) ;
-        y[1]= Math.sqrt(x[1]) ;
-        y[2]= Math.sqrt(x[2]) ;
-        y[3]= Math.sqrt(x[3]) ;
-        y[4]= Math.sqrt(x[4]) ;
-        y[5]= Math.sqrt(x[5]) ;
-        y[6]= Math.sqrt(x[6]) ;
-        y[7]= Math.sqrt(x[7]) ;
-        y[8]= Math.sqrt(x[8]) ;
-        y[9]= Math.sqrt(x[9]) ;
+    public static void main(String[] args) {
 
 
-        System.out.print("Вывод элементов массива y: ");
-        for(double element : y) {
-            System.out.print(element + ", ");
-
-        }        }
+        int a[];
+        a = new int[10];
+        for (int i = 0; i < a.length; i++) {
+            a[i] = (int) (Math.random() * 100);
+        }
+        int b[];
+        b = new int[10];
+        for (int j = 0; j < a.length; j++) {
+            b[j] = (int) Math.sqrt(a[j]);
+            System.out.println(b[j]);
+        }
+    }
 }
 
 
--- src/lesson2/Task5.java
+++ src/lesson2/Task5.java
@@ -1,12 +1,12 @@
 package lesson2;//Create a program that calculates the factorial of an integer value n.
-
 public class Task5 {
+
     public static void main(String[] args ){
         System.out.println(fuctorialN(6) );
     }
 
     public static int fuctorialN(int n)  {
-        if (n==1) {
+        if ( n == 1 || n == 0) {
             return 1;
         }
         return n*fuctorialN(n-1);
